025764def397db56e25966220ef7b1604296ad01
Tahfari Chin <167898252+weebthedev@users.noreply.github.com>
2024-12-21 07:00:45 +0000

    (shit)

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
- import { Button } from "$lib/components/ui/button";
-</script>
- 
-<Button>Click me</Button>
+    import { Button } from "$lib/components/ui/button";
+    </script>
+    
+    <div class="relative overflow-hidden py-24 lg:py-32">
+      <div class="relative z-10">
+        <div class="container py-10 lg:py-16">
+          <div class="max-w-2xl text-center mx-auto">
+            <!-- Title -->
+            <div class="mt-5 max-w-2xl">
+              <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                No slogans, <br/> just <span class="text-primary"> vlyte </span> it.
+              </h1>
+            </div>
+            <!-- End Title -->
+            <div class="mt-5 max-w-3xl">
+              
+            </div>
+            <!-- Buttons -->
+            <div class="mt-8 gap-3 flex justify-center">
+              <Button size="lg">Get started</Button>
+              <Button size="lg" variant="outline">Learn more</Button>
+            </div>
+            <!-- End Buttons -->
+          </div>
+        </div>
+      </div>
+    </div>
+    
