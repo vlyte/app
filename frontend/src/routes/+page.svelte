dbbc3a4707b91f3fb222e8fd9f852ec2c1a81eb4
myferonguilded <michaelthecrumbyt1@gmail.com>
2024-12-21 17:34:39 -0600

    add input component, revamp landing page.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,29 +1,41 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
-    </script>
-    
-    <div class="relative overflow-hidden py-24 lg:py-32">
-      <div class="relative z-10">
-        <div class="container py-10 lg:py-16">
-          <div class="max-w-2xl text-center mx-auto">
-            <!-- Title -->
-            <div class="mt-5 max-w-2xl">
-              <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                No slogans, <br/> just <span class="text-primary"> vlyte </span> it.
-              </h1>
-            </div>
-            <!-- End Title -->
-            <div class="mt-5 max-w-3xl">
-              
-            </div>
-            <!-- Buttons -->
-            <div class="mt-8 gap-3 flex justify-center">
-              <Button size="lg">Get started</Button>
-              <Button size="lg" variant="outline">Learn more</Button>
-            </div>
-            <!-- End Buttons -->
-          </div>
-        </div>
-      </div>
-    </div>
-    
+	import { Button } from '$lib/components/ui/button';
+	import Input from '$lib/components/ui/input/input.svelte';
+</script>
+
+<div class="relative overflow-hidden py-12 sm:py-16 md:py-20 lg:py-24">
+	<div class="relative z-10">
+		<div class="container px-4 sm:px-6 lg:px-8">
+			<div class="mx-auto max-w-2xl">
+				<div class="mt-5 max-w-2xl">
+					<h1
+						class="scroll-m-20 text-3xl font-extrabold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl"
+					>
+						<div
+							class="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between sm:gap-8"
+						>
+							<span>Developer profiles,</span>
+							<Button class="scroll-m-20 w-full font-extrabold tracking-tight sm:w-auto"
+								>Login</Button
+							>
+						</div>
+						<span class="text-primary mt-2 block">made simple.</span>
+					</h1>
+					<br />
+					<span class="text-slate-600"
+						><span class="text-primary">Vlyte</span> is a platform for developers to create & share their
+						profiles.</span
+					>
+					<div class="mt-8 flex flex-col gap-3 sm:flex-row">
+						<Input
+							placeholder="Email (e.g. johndoe@example.com)"
+							type="email"
+							class="w-full sm:max-w-sm"
+						/>
+						<Button size="lg" variant={'outline'} class="w-full sm:w-auto">Get started</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
