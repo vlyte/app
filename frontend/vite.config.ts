a61d5a2caaab983f02e1088a3dd48ca2e56084c0
myferonguilded <michaelthecrumbyt1@gmail.com>
2024-12-21 18:44:12 -0600

    fix vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,12 @@
 // @ts-nocheck
-
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
-import Icons from 'unplugin-icons/vite';
-// vite.config.js
 import { defineConfig } from 'vite';
-
 export default defineConfig({
 	kit: {
 		alias: {
 			$lib: path.resolve('./src/lib')
 		}
 	},
-	plugins: [
-		sveltekit(),
-		Icons({
-			compiler: 'svelte'
-		})
-	]
+	plugins: [sveltekit()]
 });
