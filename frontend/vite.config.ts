3e37fbe36263de6fc527813ceca8a72258fe49c3
myferonguilded <michaelthecrumbyt1@gmail.com>
2024-12-21 18:22:19 -0600

    add tooltip and avatar component, make frontend for dashboard page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,20 @@
 // @ts-nocheck
 
-import path from "path";
 import { sveltekit } from '@sveltejs/kit/vite';
+import Icons from 'unplugin-icons/vite';
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	kit: {
 		alias: {
-		  $lib: path.resolve("./src/lib"),
-		},
-	  },
-	plugins: [sveltekit()]
+			$lib: path.resolve('./src/lib')
+		}
+	},
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		})
+	]
 });
